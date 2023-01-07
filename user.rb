0eedfe475663b76c6ba236fea664ca239aa65821
anton <BlinovA25>
2023-01-07 19:49:31 +0300

    User class added

---
+++ user.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+# User class
+class User
+  attr_accessor :username, :chat_id, :phone_number, :first_name, :last_name, :number, :month, :year, :verification_value
+
+  def initialize(username, chat_id, phone_number = "")
+    @username = username
+    @chat_id = chat_id
+    @phone_number = phone_number
+  end
+
+  def add_cart(first_name, last_name, number, month, year, verification_value)
+    @first_name = first_name
+    @last_name = last_name
+    @number = number
+    @month = month
+    @year = year
+    @verification_value = verification_value
+  end
+end
